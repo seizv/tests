762c201a2cdc5695bedab363d363976328af0caa
SeizV <vitaliiseiz@gmail.com>
2017-02-26 22:18:31 +0200

    add simple static class

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php
 
-
-class NewTest{
-	public $text = "Hello1";
+trait TestTrait{
+    public static $traitStatic = "Test from trait";
 }
 
-$message = new NewTest();
-echo $message->text;
+class TestStatic{
+    public static $static1 = "Start";
+    public static $num = 0;
+
+    use TestTrait;
+
+    public static function setHello(){
+        self::$static1 = "Hello static";
+    }
+
+    public static function increment(){
+        self::$num++;
+    }
+}
+
+
+print TestStatic::$static1 . "<br>"; //Start <br>
+TestStatic::setHello();
+print TestStatic::$static1 . "<br>"; //Hello static <br>
+TestStatic::$static1 = "End";
+print TestStatic::$static1 . "<br>"; //End <br>
+print TestStatic::$traitStatic . "<br>"; // Test from trait
+print TestStatic::$num;     //0
+TestStatic::increment();
+TestStatic::increment();
+TestStatic::increment();
+print TestStatic::$num;     //3
